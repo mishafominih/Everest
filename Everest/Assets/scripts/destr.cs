7ce86bca3e9cc84f0c65499382b9648992e7d1f5
mishafominih <53509997+mishafominih@users.noreply.github.com>
2020-02-17 23:10:20 +0500

    добавил сосульки, солнце, появился персонаж с 1 анимацией
    впрочем, сосульки надо будет доработать после того, как юра пропишет движение персонажа(я надеюсь он уложится точно в срок :)  )

---
+++ Everest/Assets/scripts/destr.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destr : MonoBehaviour
+{
+	public GameObject Effect;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Instantiate(Effect);
+		Effect.transform.position = transform.position;
+		transform.position -= new Vector3(0, 10, 0);
+		Destroy(gameObject, 1);
+	}
+}

d4613d3d494c44a2294b5e2a230abb14d734e070
yura159 <47238316+yura159@users.noreply.github.com>
2020-02-25 19:35:07 +0500

    Доработки
    Вроде что-то сделал, но ничего не изменилось.УХУ

--- Everest/Assets/MoveScript.cs
+++ Everest/Assets/MoveScript.cs
@@ -5,23 +5,18 @@ using UnityEngine;
 
 public class MoveScript : MonoBehaviour
 {
+    public float speedAngle;
     public float speed;
-    public GameObject myCamera;
-    public GameObject player;
-
-    private Rigidbody rb;
-    private void Start()
-    {
-        rb = player.GetComponent<Rigidbody>();
-    }
+    public Transform rotate;
 
     private void FixedUpdate()
     {
-
-        if (Input.GetKeyDown(KeyCode.W))
-        { 
-            
+        if (Input.GetKey(KeyCode.W))
+        {
+            var v = new Vector3(0, 0, 1);
+            v.x = (float)(Math.Sin(rotate.rotation.eulerAngles.y * Math.PI / 180));
+            v.z = (float)(Math.Cos(rotate.rotation.eulerAngles.y * Math.PI / 180));
+            transform.position -= v * speed;
         }
-        
     }
 }

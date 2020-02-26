92ee87997de03821ba138d978ec3552a8414b66a
mishafominih <53509997+mishafominih@users.noreply.github.com>
2020-02-26 23:24:12 +0500

    полностью(или почти) прописал логику для сосулек

--- Everest/Assets/scripts/destr.cs
+++ Everest/Assets/scripts/destr.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class destr : MonoBehaviour
 {
 	public GameObject Effect;
+	Rigidbody rb;
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.useGravity = false;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Terrarian")
+		{
+			var a = GetComponents<BoxCollider>().Where(x => x.isTrigger == false)
+				.First().isTrigger = true;
+			Instantiate(Effect);
+			Effect.transform.position = transform.position;
+			transform.position -= new Vector3(0, 10, 0);
+			Destroy(gameObject, 1);
+		}
+	}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		Instantiate(Effect);
-		Effect.transform.position = transform.position;
-		transform.position -= new Vector3(0, 10, 0);
-		Destroy(gameObject, 1);
+		if (other.tag == "Player")
+		{
+			rb.useGravity = true;
+		}
 	}
 }

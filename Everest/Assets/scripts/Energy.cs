9b5bd445555ac1c1b60ae86cf19459448d9e8d58
yura159 <47238316+yura159@users.noreply.github.com>
2020-03-22 14:24:13 +0500

    Energy and Oxygen
    Добавил шкалу жизненных показателей: энергию и кислород.

---
+++ Everest/Assets/scripts/Energy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float energy;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = energy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
